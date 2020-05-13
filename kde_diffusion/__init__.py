b2dd0b4b415e6d8342e34c9bc4b89363b2ceb817
John Hennig <John.Hennig@Comet.ch>
2020-05-13 04:22:44 +0200

    Put the project's one-liner description back at the top so that Flit finds it.

--- kde_diffusion/__init__.py
+++ kde_diffusion/__init__.py
@@ -1,10 +1,10 @@
 ﻿"""
-Exposes the library's API and hold its meta information.
-"""
+Kernel density estimation via diffusion in 1d and 2d.
 
-# Public interface
-from .kde1d import kde1d
-from .kde2d import kde2d
+This module holds meta information and exposes the library's public
+interface. The actual functionality is implemented in separate
+modules.
+"""
 
 # Meta information
 __title__     = 'KDE-diffusion'
@@ -14,3 +14,7 @@ __date__      = '2020–05–13'
 __author__    = 'John Hennig'
 __copyright__ = 'John Hennig'
 __license__   = 'MIT'
+
+# Public interface
+from .kde1d import kde1d
+from .kde2d import kde2d
