fc4dfbbb468c5f5b63328562d8a4598f185914a0
John Hennig <John.Hennig@Comet.ch>
2020-05-13 03:50:15 +0200

    Bumped version for release 0.9.1, with minor updates.

--- kde_diffusion/__init__.py
+++ kde_diffusion/__init__.py
@@ -9,8 +9,8 @@ from .kde2d import kde2d
 # Meta information
 __title__     = 'KDE-diffusion'
 __synopsis__  = 'Kernel density estimation via diffusion.'
-__version__   = '0.9'
-__date__      = '2020–05–12'
+__version__   = '0.9.1'
+__date__      = '2020–05–13'
 __author__    = 'John Hennig'
 __copyright__ = 'John Hennig'
 __license__   = 'MIT'
