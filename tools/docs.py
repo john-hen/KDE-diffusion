40ab2f5a17840aa92593b4a4f8eba4c2ba328dfc
John Hennig <jhen@gmx.net>
2022-02-25 19:44:30 +0100

    Updated dev tools.

--- tools/docs.py
+++ tools/docs.py
@@ -1,9 +1,9 @@
-﻿"""Renders the documentation."""
+﻿"""Builds the documentation locally."""
 
 from subprocess import run
 from pathlib import Path
 
 root = Path(__file__).resolve().parent.parent
-process = run(['sphinx-build', 'docs', 'deploy/docs'], cwd=root)
+process = run(['sphinx-build', 'docs', 'tools/docs'], cwd=root)
 if process.returncode:
     raise RuntimeError('Error while rendering documentation.')
