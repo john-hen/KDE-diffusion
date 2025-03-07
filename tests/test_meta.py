201ae6223f5fe72c8fdcc99a6b286a50eaec0ac5
John Hennig <jhen@gmx.net>
2025-03-07 18:00:28 +0100

    Don't use dynamic package metadata.
    Having dynamic metadata means that the build backend (Flit) has to build
    the package even for an editable install just to access metadata such as
    the version number. That is, it installs all dependencies into the build
    environment and then imports the package. This is more error-prone than
    it needs to be, especially when, as the case is now, the import already
    fails because a dependency broke. This way, we can at least install the
    package for local development, to then see the failure when running the
    test suite.

--- tests/test_meta.py
+++ tests/test_meta.py
@@ -5,15 +5,6 @@ import re
 
 
 def test_meta():
-    fields = ['title', 'synopsis', 'version', 'date', 'author', 'copyright',
-              'license']
-    for field in fields:
-        assert hasattr(meta, field)
-        assert isinstance(getattr(meta, field), str)
-    assert meta.title == 'KDE-diffusion'
-    assert meta.synopsis
+    assert meta.name == 'KDE-diffusion'
     assert re.match(r'\d+\.\d+\.\d+', meta.version)
-    assert re.match(r'\d\d\d\d–\d\d–\d\d', meta.date)
-    assert meta.author
-    assert meta.copyright
-    assert meta.license
+    assert meta.summary
